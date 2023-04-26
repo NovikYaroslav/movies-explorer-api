b44ac0b6ef906e29ef4ec7314afdc53036c48b2f
Novik Yaroslav <yaroslavnowik@yandex.ru>
2023-04-26 14:30:53 +0300

    deploy

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
+const limiter = require('./utils/limiter');
 const router = require('./routers');
 const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { MONGO_ADRESS } = require('./utils/const');
 
 const { PORT = 3001 } = process.env;
-// const allowedCors = [
-//   'тут будет домен https',
-//   'тут будет домен http',
-// ];
+const allowedCors = [
+  'https://movies.novik.nomoredo.nomoredomains.monster',
+  'http://movies.novik.nomoredo.nomoredomains.monster',
+];
 
-// const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 200,
-});
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
 const app = express();
 
 mongoose.set('strictQuery', false);
@@ -44,21 +41,21 @@ app.use(helmet());
 app.use(limiter);
 app.use(requestLogger);
 // eslint-disable-next-line consistent-return
-// app.use((req, res, next) => {
-//   const { method } = req;
-//   const { origin } = req.headers;
-//   const requestHeaders = req.headers['access-control-request-headers'];
-//   res.header('Access-Control-Allow-Credentials', true);
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//   }
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     return res.end();
-//   }
-//   next();
-// });
+app.use((req, res, next) => {
+  const { method } = req;
+  const { origin } = req.headers;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  res.header('Access-Control-Allow-Credentials', true);
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  next();
+});
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
